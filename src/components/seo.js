c3a6d6063456816d12ccbce2acc4fb83627ac84c
Kelvin Nunez <knunezweb@gmail.com>
2022-05-10 18:44:41 -0400

    Fixed SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,8 +1,8 @@
-import * as React from "react"
+import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function Seo({ description, lang, meta, title }) {
+const Seo = ({ description, lang, meta, title })  => {
   const { site } = useStaticQuery(
     graphql`
       query {
