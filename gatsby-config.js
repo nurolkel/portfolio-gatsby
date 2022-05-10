65f1d227c3b281b17bf9e54e8d796a0c2931988e
Kelvin Nunez <knunezweb@gmail.com>
2022-05-10 14:09:15 -0400

    Added Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,41 +17,25 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-emotion`,
       options: {
         // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
         // The values for each key in this example are the defaults the plugin uses.
         sourceMap: true,
-        autoLabel: "dev-only",
+        autoLabel: "always",
         labelFormat: `[local]`,
         cssPropOptimization: true,
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-robots-txt',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon-16x16.png`, // This path is relative to the root of the site.
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-      },
-      
+        host: 'https://www.example.com',
+        sitemap: 'https://www.example.com/sitemap/sitemap-index.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
     },
     {
       resolve: `gatsby-source-sanity`,
@@ -66,5 +50,36 @@ module.exports = {
         // use `graphqlTag` to specify the tag name. Defaults to `default`.
       },
     }
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Kelvin-Nunez-Portfolio`,
+        short_name: `Kelvin-Nunez-Portfolio`,
+        start_url: `/`,
+        background_color: `#3500db`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `standalone`,
+        icon: `src/images/android-chrome-512x512.png`, // This path is relative to the root of the site.
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+        legacy: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`, `/projects`, `/contact`],
+      },
+    },
   ],
 }
